422b1dbc68e97139b3870252c015f3588c0c52d2
Vikas Pejaver <vikaspejaver@h4tg-pejavv01.anbg.mssm.edu>
2022-03-31 15:02:51 -0400

    Added extensive comments for the posterior probability plotting code

--- plotting/plot_both_posteriors_pub.m
+++ plotting/plot_both_posteriors_pub.m
@@ -1,97 +1,121 @@
-function lgnd = plot_both_posteriors (postp, postb, thrs, Post_p, Post_b, method, flab, eshade)
+function lgnd = plot_both_posteriors_pub(postp, postb, thrs, Post_p, Post_b, method, flab, eshade)
 
-offset = max(thrs) - min(thrs);
-if ~isempty(Post_p) % pathogenic
-    %subplot(2, 1, 1, 'Parent', P);
-    % make a plot
+%% Function to plot local posterior probabilities
+% Note: (1) based heavily on 'plot_both_posteriors.m' in the 
+%           'local_posterior_probability' directory
+%       (2) there is a lot of hardcoded positional information here
+%           (change at your own peril!)
+
+% postp = posterior for pathogenicity
+% postb = posterior for benignity
+% thrs = thresholds to be examined
+% Post_p = pathogenic posterior probability thresholds corresponding to the 4 strengths
+% Post_b = benign posterior probability thresholds
+% method = method name
+% flab = A-Z figure labels (for publication purposes)
+% eshade = information for "error" bars (gray curves in paper)
+% lgnd = legend data structure is returned
+
+% initialize
+offset = max(thrs) - min(thrs); % set offset value based on score range
+
+% pathogenic
+if ~isempty(Post_p) % this check differentiates pathogenic from benign
+
+    % plot the point estimate (although it says 'mean' it is using the full 
+    % data set)
     h = plot(thrs, mean(postp, 1), 'color', 'black', 'linewidth', 1);
     hold on;
 
+    % plot confidence bound curve
     if ~isempty(eshade{1}) & ~isempty(eshade{2})
         uci = eshade{1};
-	lci = eshade{2};
-	plot(thrs, uci, '-', 'color', '#808080', 'linewidth', 1);
-	plot(thrs, lci, '-', 'color', '#808080', 'linewidth', 1);
-	%area(thrs, uci, 'facecolor', [0, 0, 0], 'facealpha', 0.05, 'edgecolor', 'black');
-    	%area(thrs, lci, 'facecolor', [1, 1, 1], 'edgecolor', 'black');
+        lci = eshade{2};
+        plot(thrs, uci, '-', 'color', '#808080', 'linewidth', 1);
+        plot(thrs, lci, '-', 'color', '#808080', 'linewidth', 1);
     elseif ~isempty(eshade{1})
-    	uci = eshade{1};
-	plot(thrs, uci, '-', 'color', '#808080', 'linewidth', 1);
-        %area(thrs, uci, 'facecolor', [0, 0, 0], 'facealpha', 0.05, 'edgecolor', 'black');
+        uci = eshade{1};
+        plot(thrs, uci, '-', 'color', '#808080', 'linewidth', 1);
     end
 
+    % plot lines corresponding to posterior probability strength thresholds
     plot(thrs, ones(1, length(thrs)) * Post_p(1), '-', 'color', 'red', 'linewidth', 2);
-    %text(min(thrs) +  0.01*offset, Post_p(1) - 0.02, 'VSt', 'fontsize', 8, 'color', 'red');
     plot(thrs, ones(1, length(thrs)) * Post_p(2), '-.', 'color', 'red', 'linewidth', 2);
-    %text(min(thrs) +  0.01*offset, Post_p(2) - 0.02, 'St', 'fontsize', 8, 'color', 'red');
     plot(thrs, ones(1, length(thrs)) * Post_p(3), '--', 'color', 'red', 'linewidth', 2);
-    %text(min(thrs) +  0.01*offset, Post_p(3) - 0.02, 'M', 'fontsize', 8, 'color', 'red');
     plot(thrs, ones(1, length(thrs)) * Post_p(4), ':', 'color', 'red', 'linewidth', 2);
-    %text(min(thrs) +  0.01*offset, Post_p(4) - 0.02, 'Su', 'fontsize', 8, 'color', 'red');
 
+    % set axis labels
     axis([min(thrs) max(thrs) 0 1]);
     if strcmp(method, 'SIFT') | strcmp(method, 'FATHMM')
         xlabel('-Score');
     else
-	xlabel('Score');
+    	xlabel('Score');
     end
     ylabel('Posterior');
+
+    % set x-axis tick information
     set(gca, 'Xtick', min(thrs):offset/5:max(thrs));
     set(gca, 'XtickLabel', strsplit(num2str(min(thrs):offset/5:max(thrs), '%.1f '), ' ')); %min(thrs):offset/5:max(thrs));
-    set(gca, 'XtickLabelRotation', 45);	
+    set(gca, 'XtickLabelRotation', 45);
+
+    % set y-axis tick information
     set(gca, 'Ytick', 0:1/5:1);
     set(gca, 'YtickLabel', strsplit(num2str(0:1/5:1, '%.1f '), ' '));
-    %pbaspect([1.5, 1, 1]);
+    
+    % set title and gridline
     subtitle(method);
     set(gca, 'TitleHorizontalAlignment', 'left');
     text(min(thrs)-1.5*offset/5, 1.2, sprintf('%s', flab), 'fontsize', 16); %, 'fontweight', 'Bold');
     grid on;
+    
+    % set legend but don't show here
     lgnd = legend({'Point estimate', '', 'PP3\_VeryStrong', 'PP3\_Strong', 'PP3\_Moderate', 'PP3\_Supporting'}, 'location', 'west');
     set(lgnd, 'visible', 'off')
     hold off;
-else   % benign
-    %subplot(2, 1, 2, 'Parent', P)
-    thrs = thrs(length(thrs) : -1 : 1);
+else   
+    % benign
+    thrs = thrs(length(thrs) : -1 : 1); % invert order of thresholds
 
-    % make a plot
+    % plot the point estimate (although it says 'mean' it is using the full 
+    % data set)
     h = plot(thrs, mean(postb, 1), 'color', 'black', 'linewidth', 1);
     hold on;
 
+    % plot confidence bound curve
     if ~isempty(eshade{1}) & ~isempty(eshade{2})
         uci = eshade{1};
-	lci = eshade{2};
-	plot(thrs, uci, '-', 'color', '#808080', 'linewidth', 1);
-	plot(thrs, lci, '-', 'color', '#808080', 'linewidth', 1);
-	%area(thrs, uci, 'facecolor', [0, 0, 0], 'facealpha', 0.05, 'edgecolor', 'black');
-    	%area(thrs, lci, 'facecolor', [1, 1, 1], 'edgecolor', 'black');
+    	lci = eshade{2};
+    	plot(thrs, uci, '-', 'color', '#808080', 'linewidth', 1);
+    	plot(thrs, lci, '-', 'color', '#808080', 'linewidth', 1);
     elseif ~isempty(eshade{2})
         lci = eshade{2};
-	plot(thrs, lci, '-', 'color', '#808080', 'linewidth', 1);
-    	%area(thrs, lci, 'facecolor', [1, 1, 1], 'edgecolor', 'black');
+    	plot(thrs, lci, '-', 'color', '#808080', 'linewidth', 1);
     end
 
+    % plot lines corresponding to posterior probability strength thresholds
     plot(thrs, ones(1, length(thrs)) * Post_b(1), '-', 'color', [0, 0.4470, 0.7410], 'linewidth', 2);
-    %text(max(thrs) - 0.07*offset, Post_b(1) - 0.0005, 'VSt', 'fontsize', 8, 'color', [0, 0.4470, 0.7410]);
     plot(thrs, ones(1, length(thrs)) * Post_b(2), '-.', 'color', [0, 0.4470, 0.7410], 'linewidth', 2);
-    %text(max(thrs) - 0.045*offset, Post_b(2) - 0.0005, 'St', 'fontsize', 8, 'color', [0, 0.4470, 0.7410]);
     plot(thrs, ones(1, length(thrs)) * Post_b(3), '--', 'color', [0, 0.4470, 0.7410], 'linewidth', 2);
-    %text(max(thrs) - 0.04*offset, Post_b(3) - 0.0005, 'M', 'fontsize', 8, 'color', [0, 0.4470, 0.7410]);
     plot(thrs, ones(1, length(thrs)) * Post_b(4), ':', 'color', [0, 0.4470, 0.7410], 'linewidth', 2);
-    %text(max(thrs) - 0.055*offset, Post_b(4) - 0.0005, 'Su', 'fontsize', 8, 'color', [0, 0.4470, 0.7410]);
 
+    % set axis labels (y-axis not labeled for benigns)
     axis([min(thrs) max(thrs) 0.975 1]);
     if strcmp(method, 'SIFT') | strcmp(method, 'FATHMM')
         xlabel('-Score');
     else
-	xlabel('Score');
+    	xlabel('Score');
     end
-    %ylabel('Posterior');
+
+    % set x-axis tick information
     set(gca, 'Xtick', min(thrs):offset/5:max(thrs));
     set(gca, 'XtickLabel', strsplit(num2str(min(thrs):offset/5:max(thrs), '%.1f '), ' ')); %min(thrs):offset/5:max(thrs));
     set(gca, 'XtickLabelRotation', 45);
+    
+    % set y-axis tick information
     set(gca, 'Ytick', 0.975:(1-0.975)/5:1);
     set(gca, 'YtickLabel', strsplit(num2str(0.975:(1-0.975)/5:1, '%.3f '), ' ')); %0.975:(1-0.975)/5:1);
-    %pbaspect([1.5, 1, 1]);
+    
+    % set legend but don't show here
     grid on;
     lgnd = legend({'', 'One-sided confidence bound', 'BP4\_VeryStrong', 'BP4\_Strong', 'BP4\_Moderate', 'BP4\_Supporting'}, 'location', 'west');
     set(lgnd, 'visible', 'off');
--- plotting/plot_posterior_wrapper.m
+++ plotting/plot_posterior_wrapper.m
@@ -1,63 +1,71 @@
-% Vikas Pejaver and Pedja Radivojac
-% December 2020
-% University of Washington and Northeastern University
-% Script to select thresholds based on posteriors and bootstrapped data
+%% Vikas Pejaver and Predrag Radivojac
+% Icahn School of Medicine at Mount Sinai, Univerity of Washington, and 
+% Northeastern University
+% 2021-2022
 
+%% Wrapper script to plot publication-ready version of local posterior 
+%% probability plots (Figure 3)
+% Input files: (1) Path to MAT files output by main.m in 
+%                  'local_posterior_probability' with the filename regular 
+%                  expression (see below). Files located in
+%                  'results -> bootstrapped'
+%              (2) Directory that contains ClinVar 2019 and gnomAD data 
+%                  sets, 'data -> data_combined_tools_split' (this is 
+%                  mainly used to obtain all unique thresholds for a given
+%                  tool as it is not stored in the output of main.m)
+% Output file: A single EPS image file containing plots for all 13 pairs of
+%              plots (output format can be changed as per MATLAB
+%              documentation)
+ 
+%% Initialize
+% clear screen and any standing variables in MATLAB workspace
 clear
 clc
 
 %% Constants and defaults
 infiles = '/Users/vikaspejaver/Desktop/ClinGen-SVI/results/new_bootstrapping/final_10k_3prct_100pts*.mat';
+indir = '/Users/vikaspejaver/Desktop/ClinGen-SVI/data/data_combined_tools_split/'; % change as needed
 out_file = 'posterior_plots_13tools.eps';
 toolnames = {'BayesDel-noAF', 'CADDphred', 'EA1.0', 'FATHMM', 'GERP++', 'MPC', 'MutPred2.0', 'phyloP100way', 'PolyPhen2-HVAR', 'PrimateAI', 'REVEL', 'SIFT', 'VEST4'};
 titles = {'BayesDel', 'CADD', 'EA', 'FATHMM', 'GERP++', 'MPC', 'MutPred2', 'PhyloP', 'PolyPhen-2', 'PrimateAI', 'REVEL', 'SIFT', 'VEST4'};
 fig_labels = 'A':'Z';
 fig_labels = fig_labels(1:length(titles));
-%toolnames = {'BayesDel-noAF', 'CADDphred', 'EA1.0', 'FATHMM', 'GERP++', 'MPC', 'MutPred2.0', 'PolyPhen2-HVAR', 'PrimateAI', 'REVEL', 'SIFT', 'VEST4'};
-%titles = {'BayesDel', 'CADD', 'EA', 'FATHMM', 'GERP++', 'MPC', 'MutPred2', 'PolyPhen-2', 'PrimateAI', 'REVEL', 'SIFT', 'VEST4'};
-shaded = 'CI'; % SE, CI or None
-nrows = 7; %7
+shaded = 'CI'; % 'SE' - standard errors, 'CI' - confidence interval-based, or 'None' - no gray line in the plot
+onesided = 5; % If 'CI' is chosen, the percentage confidence interval that is calculated
+nrows = 7;
 ncols = 4;
 
 %% Prep figure
 f = figure('units', 'normalized', 'outerposition', [0 0 0.33 1]);
 P = uipanel('Parent', f, 'BorderType', 'none');
 
-%% Loop through each MAT file and get information
+%% Loop through each MAT file and plot
 filenames = strcat({dir(infiles).folder}, '/', {dir(infiles).name})';
 for n = 1:length(filenames)
     n
-    % Load MAT file
+    % load MAT file (a single file may contain results for multiple tools)
     load(filenames{n});
 
-    % Get indices of the methods that were done
+    % get indices of the methods that were done (this was used only for
+    % development purposes when results were partially done)
     idx = find(~cellfun(@isempty, ThresholdP));
 
-    % Plot posteriors
+    % loop through each tool 
     for i = idx
-
-    	if ispc()
-            D = load(['C:\Users\Predrag Radivojac\iCloudDrive\ClinGen\New Predictions\' files{i}]);
-    	else
-	    D = load(['/Users/vikaspejaver/Desktop/ClinGen-SVI/data/new_predictions_for_pedja/' files{i}]);
-    	end
-    
-	fprintf(1, '\n%s', files{i});
+    	% read in ClinVar variants
+        D = load([indir files{i}]);
+	    fprintf(1, '\n%s', files{i});
     	fprintf(1, '\n');
     
-	x = D(:, 1);
+	    x = D(:, 1);
     	y = D(:, 2);
     
-	if ispc()
-            file = strrep(['C:\Users\Predrag Radivojac\iCloudDrive\ClinGen\New Predictions\' files{i}], 'BLB', 'U');
-    	else
-	    file = strrep(['/Users/vikaspejaver/Desktop/ClinGen-SVI/data/new_predictions_for_pedja/' files{i}], 'BLB', 'U');
-    	end
-    
-	D = load(file);
+        % read in gnomAD variants
+	    file = strrep([indir files{i}], 'BLB', 'U');
+	    D = load(file);
     	g = D(D(:, 2) == 0, 1);
     
-	% negate the scores for methods where high prediction means benign
+	    % negate the scores for methods where high prediction means benign
     	if strcmp(methods{i}, 'SIFT') == 1 || strcmp(methods{i}, 'FATHMM') == 1
             x = -x;
             g = -g;
@@ -70,36 +78,39 @@ for n = 1:length(filenames)
 
         % thresholds for posterior for pathogenicity, reverse sorted
         thrs = flip(unique([x; g; floor(min([x; g])); ceil(max([x; g]))]));
+        
+        % calculate "error bars"; here represented as a grey line around
+        % the point estimate
+	    if strcmp(shaded, 'SE') % these will be symmetric
+	        perrors = {posteriors_p{i}(1, :) - std(posteriors_p{i}(2:end, :)), posteriors_p{i}(1, :) + std(posteriors_p{i}(2:end, :))};
+	        berrors = {posteriors_b{i}(1, :) - std(posteriors_b{i}(2:end, :)), posteriors_b{i}(1, :) + std(posteriors_b{i}(2:end, :))};
+	    elseif strcmp(shaded, 'CI') % these are set so that the CI-based threshold is more stringent than the point estimate
+            perrors = {prctile(posteriors_p{i}(2:end, :), onesided), []};
+            berrors = {[], prctile(posteriors_b{i}(2:end, :), onesided)};
+	    else
+	        perrors = {[], []};
+	        berrors = {[], []};
+	    end
 
-	if strcmp(shaded, 'SE')
-	    perrors = {posteriors_p{i}(1, :) - std(posteriors_p{i}(2:end, :)), posteriors_p{i}(1, :) + std(posteriors_p{i}(2:end, :))};
-	    berrors = {posteriors_b{i}(1, :) - std(posteriors_b{i}(2:end, :)), posteriors_b{i}(1, :) + std(posteriors_b{i}(2:end, :))};
-	elseif strcmp(shaded, 'CI')
-            perrors = {prctile(posteriors_p{i}(2:end, :), 5), []};
-            berrors = {[], prctile(posteriors_b{i}(2:end, :), 5)};
-	else
-	    perrors = {[], []};
-	    berrors = {[], []};
-	end
+    	% get correct tool name (title)
+    	inds = find(strcmp(toolnames, methods{i}));
+    	this_title = titles{inds};
 
-	% plot
-	inds = find(strcmp(toolnames, methods{i}));
-	this_title = titles{inds};
-	subplot(nrows, ncols, 2*inds-1, 'Parent', P);
-	lp = plot_both_posteriors_pub(posteriors_p{i}(1, :), posteriors_b{i}(1, :), thrs, Post_p, [], this_title, fig_labels(inds), perrors);
-	subplot(nrows, ncols, 2*inds, 'Parent', P);
-	lb = plot_both_posteriors_pub(posteriors_p{i}(1, :), posteriors_b{i}(1, :), thrs, [], Post_b, this_title, '', berrors);
-	%end
-	%break;
+        % plot pathogenic
+    	subplot(nrows, ncols, 2*inds-1, 'Parent', P);
+    	lp = plot_both_posteriors_pub(posteriors_p{i}(1, :), posteriors_b{i}(1, :), thrs, Post_p, [], this_title, fig_labels(inds), perrors);
+    	
+        % plot benign
+        subplot(nrows, ncols, 2*inds, 'Parent', P);
+    	lb = plot_both_posteriors_pub(posteriors_p{i}(1, :), posteriors_b{i}(1, :), thrs, [], Post_b, this_title, '', berrors);
     end
-    %break;
 end
 
-% Add legends
+% add legends (these are shown only once for the full figure)
 set(lp, 'visible', 'on');
 set(lp, 'position', [0.5240 0.131 0.1448 0.0570]);
 set(lb, 'visible', 'on');
 set(lb, 'position', [0.6870 0.131 0.2152 0.0570]);
 
-% Print to file
+% print to file
 exportgraphics(P, out_file);
